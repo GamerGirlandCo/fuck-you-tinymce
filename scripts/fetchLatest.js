bcdaae9743a62dc4172a117471bd3a49462d90be
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-01-10 14:28:22 -0500

    add mediaembed css

--- scripts/fetchLatest.js
+++ scripts/fetchLatest.js
@@ -5,7 +5,7 @@ import {resolve, dirname} from "path";
 const freeKey = "qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc";
 const skins = ["dark", "default", "document", "tinymce-5", "tinymce-5-dark", "writer"];
 const uis = ["oxide", "oxide-dark", "tinymce-5", "tinymce-5-dark"];
-const pluginList = "a11ychecker advcode advtable advtemplates typography ai casechange checklist comments editimage mediaembed export footnotes formatpainter inlinecss linkchecker mentions mergetags moxiemanager pageembed permanentpen powerpaste rtc spellchecker autocorrect tableofcontents tinydrive accordion advlist anchor autolink autoresize autosave charmap code codesample directionality emoticons fullscreen help image importcss insertdatetime link lists media nonbreaking pagebreak preview quickbars save searchreplace table template visualblocks visualchars wordcount";
+const pluginList = "a11ychecker advcode advtable advtemplate typography ai casechange checklist comments editimage mediaembed export footnotes formatpainter inlinecss linkchecker mentions mergetags moxiemanager pageembed permanentpen powerpaste rtc spellchecker autocorrect tableofcontents tinydrive accordion advlist anchor autolink autoresize autosave charmap code codesample directionality emoticons fullscreen help image importcss insertdatetime link lists media nonbreaking pagebreak preview quickbars save searchreplace table template visualblocks visualchars wordcount";
 const base = `https://cdn.tiny.cloud/1/${freeKey}/tinymce/6`
 
 let headers = {
@@ -30,6 +30,7 @@ async function main() {
 		await fetchAux("/plugins/powerpaste/js/wordimport.js");
 		await fetchAux("/plugins/a11ychecker/css/annotations.css");
 		await fetchAux("/plugins/pageembed/css/empa30.css");
+		await fetchAux("/plugins/mediaembed/content.min.css");
 		for(let t of skins) {
 			await fetchAux(`/skins/content/${t}/content.min.css`)
 		}
